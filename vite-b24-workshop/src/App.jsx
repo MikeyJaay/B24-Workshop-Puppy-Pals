50a151d0f4edd6ac3835592fde64e4a4538348cf
Michael  Jaroszynski <mikeyj@Mikey-Js-MacBook-Pro.local>
2024-02-19 14:39:30 -0500

    added chaning id p tag

--- vite-b24-workshop/src/App.jsx
+++ vite-b24-workshop/src/App.jsx
@@ -9,14 +9,20 @@ import { useState } from "react";
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
   console.log("puppyList: ", puppyList);
+  // adding FeatPupID
+  const [featPupId, setFeatPupId] = useState(null);
   return (
     <>
       <div className="App">
-        {
-        puppies.map((puppy) => {
-          return <p>{puppy.name}</p>;
-        })
-        }
+
+        {puppies.map((puppy) => {
+          return (
+            <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>
+              {puppy.name}
+            </p>
+          );
+        })} 
+       {featPupId && <p>Featured Puppy ID: {featPupId}</p>}
       </div>
     </>
   );
