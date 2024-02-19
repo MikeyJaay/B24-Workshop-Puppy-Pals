f4bd91e56c16ae94ca510aadafb60f1410675999
Michael  Jaroszynski <mikeyj@Mikey-Js-MacBook-Pro.local>
2024-02-19 14:16:18 -0500

    added useState

--- vite-b24-workshop/src/App.jsx
+++ vite-b24-workshop/src/App.jsx
@@ -1,8 +1,15 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+// Importing puppyList array
+import {puppyList} from './data.js'
+// Import use State Hook
+import { useState } from 'react'
+
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log('useState', useState)
   return (
     <>
       <div></div>
