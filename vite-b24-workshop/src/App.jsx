a0f12a43ce660c15fb6b5eb78807882552643f8c
Michael  Jaroszynski <mikeyj@Mikey-Js-MacBook-Pro.local>
2024-02-19 14:22:18 -0500

    added .map to puppy array

--- vite-b24-workshop/src/App.jsx
+++ vite-b24-workshop/src/App.jsx
@@ -2,17 +2,22 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 // Importing puppyList array
-import {puppyList} from './data.js'
+import { puppyList } from "./data.js";
 // Import use State Hook
-import { useState } from 'react'
-
+import { useState } from "react";
 
 function App() {
-  const [puppies, setPuppies] = useState(puppyList)
-  console.log('useState', useState)
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log("puppyList: ", puppyList);
   return (
     <>
-      <div></div>
+      <div className="App">
+        {
+        puppies.map((puppy) => {
+          return <p>{puppy.name}</p>;
+        })
+        }
+      </div>
     </>
   );
 }
